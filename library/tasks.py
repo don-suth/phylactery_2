fb20023cf710c6ac749d532cc74b5f0d6d1af4f1
donsuth <donald@sutherland.id.au>
2025-03-05 12:47:46 +0800

    Add task to fix unused reservations

--- library/tasks.py
+++ library/tasks.py
@@ -1,7 +1,22 @@
 from celery import shared_task
 from phylactery.communication.email import render_html_email, send_single_email_task
 from django.utils import timezone
+from library.models import Reservation
 
+
+@shared_task(name="cleanup_permissions")
+def check_for_unused_reservations():
+	"""
+	Check for reservations that are active, but their borrow date has passed.
+	Sets them to in-active, so that the Items in that reservation can be borrowed normally.
+	Intended to be run every day.
+	"""
+	active_reservations = Reservation.objects.filter(
+		is_active=True,
+		requested_date_to_borrow__lt=timezone.localdate()
+	)
+	active_reservations.update(is_active=False)
+	
 
 def send_borrow_receipt(email_address, borrower_name, items, authorised_by):
 	context = {
