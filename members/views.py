bd7d05c7d69da018249f78209bfa3a532ce5f89d
donsuth <donald@sutherland.id.au>
2025-09-03 11:35:10 +0800

    Add a gatekeeper list, accessible to gatekeepers

--- members/views.py
+++ members/views.py
@@ -5,7 +5,7 @@ from django.http.response import Http404
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, TemplateView, DetailView, FormView
-from .models import Member, RankChoices
+from .models import Member, RankChoices, Rank
 from .decorators import gatekeeper_required, potential_superuser_required,  superuser_required
 from .forms import ChangeEmailPreferencesForm, ToggleSuperuserForm
 
@@ -20,6 +20,30 @@ class MemberListView(ListView):
 	
 	def get_queryset(self):
 		qs = Member.objects.all()
+		
+		self.search_query = self.request.GET.get("search")
+		if self.search_query:
+			qs = qs.filter(Q(short_name__icontains=self.search_query) | Q(long_name__icontains=self.search_query))
+		
+		return qs
+	
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+		context["search_query"] = self.search_query
+		return context
+
+
+@method_decorator(gatekeeper_required, name="dispatch")
+class GatekeeperListView(ListView):
+	model = Member
+	paginate_by = 50
+	template_name = "members/gatekeeper_list.html"
+	
+	search_query = None
+	
+	def get_queryset(self):
+		active_gatekeeper_ranks = Rank.objects.all_active().filter(rank_name=RankChoices.GATEKEEPER)
+		qs = Member.objects.filter(ranks__in=active_gatekeeper_ranks)
 		
 		self.search_query = self.request.GET.get("search")
 		if self.search_query:
