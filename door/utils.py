c9818a276358de87a78f669865a16c51227ceb7e
Donald Sutherland <donald@sutherland.id.au>
2025-12-31 17:50:17 +0800

    Add some placeholder utility function

--- door/utils.py
+++ door/utils.py
@@ -11,3 +11,33 @@ def get_door_status():
 	door_status = redis_connection.get("door:status")
 	return door_status
 
+
+def redis_open_door():
+	"""
+	Updates Redis to open the Door.
+	This involves:
+		1) Changing the status of the "door:status" key.
+		2) Publishing the status on the Pubsub channel "door:updates".
+		3) Adding an entry to the Redis stream with:
+			- timestamp
+			- new status
+			- member id
+			- member name
+			- source (phylactery/lich)
+	"""
+	pass
+
+def redis_close_door():
+	"""
+	Updates Redis to close the Door.
+	This involves:
+		1) Changing the status of the "door:status" key.
+		2) Publishing the status on the Pubsub channel "door:updates".
+		3) Adding an entry to the Redis stream with:
+			- timestamp
+			- new status
+			- member id
+			- member name
+			- source (phylactery/lich)
+	"""
+	pass
