f69167850b8bb32e9931820a6f997d6b3acc070b
Donald Sutherland <donald@sutherland.id.au>
2025-12-31 16:49:17 +0800

    Add a new DoorEvent model

---
+++ door/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class EventChoices(models.TextChoices):
+	OPENED = "OPENED", "Door Opened"
+	CLOSED = "CLOSED", "Door Closed"
+
+
+class DoorEvent(models.Model):
+	"""
+	A model for logging Door Events.
+	Each instance represents one event of the door either opening or closing.
+	"""
+	event_time = models.DateTimeField()
+	event_type = models.CharField(
+		max_length=6,
+		choices=EventChoices.choices
+	)
+	previous_event = models.OneToOneField(
+		"DoorEvent",
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True,
+		related_name="next_event"
+	)
+	member = models.ForeignKey(
+		"members.Member",
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True
+	)
+	notes = models.JSONField()
+	
+	def __str__(self):
+		return f"{self.event_time} - {self.event_type}"
