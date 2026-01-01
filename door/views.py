4f06637b717dc0111fba4838abafdb0fe6608084
Donald Sutherland <donald@sutherland.id.au>
2026-01-01 18:37:53 +0800

    Make the form views functional

--- door/views.py
+++ door/views.py
@@ -1,9 +1,11 @@
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, FormView
 
 from members.decorators import gatekeeper_required
 from door.forms import OpenCloseDoorForm
-from door.utils import get_door_status
+from door.utils import get_door_status, redis_open_door, redis_close_door
 
 
 class DoorStatusView(TemplateView):
@@ -19,9 +21,21 @@ class DoorStatusView(TemplateView):
 class OpenDoorFormView(FormView):
 	form_class = OpenCloseDoorForm
 	template_name = "door/open_door_form.html"
+	
+	def form_valid(self, form):
+		member = self.request.unigames_member
+		redis_open_door(member.pk, member.short_name)
+		messages.success(self.request, "You have opened Unigames!")
+		return redirect("door:status")
 
 
 @method_decorator(gatekeeper_required, name="dispatch")
 class CloseDoorFormView(FormView):
 	form_class = OpenCloseDoorForm
 	template_name = "door/close_door_form.html"
+	
+	def form_valid(self, form):
+		member = self.request.unigames_member
+		redis_close_door(member.pk, member.short_name)
+		messages.success(self.request, "You have closed Unigames!")
+		return redirect("door:status")
