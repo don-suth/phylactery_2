70c65ef2fec307587931d6413b2c19c451c658b6
donsuth <donald@sutherland.id.au>
2026-02-16 18:18:40 +0800

    Most of the logic for the wizard is completed

--- door/wizards.py
+++ door/wizards.py
@@ -1,6 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.shortcuts import redirect
 from formtools.wizard.views import SessionWizardView
+
+from door.utils import is_cameron_hall_open, redis_check_cooldown, publish_letmein_request
 
 
 @method_decorator(login_required, name="dispatch")
@@ -18,10 +22,25 @@ class LetMeInWizard(SessionWizardView):
 		("request", None),
 	]
 	condition_dict = {
-		"confirm": True
+		"confirm": is_cameron_hall_open
 	}
 	template_name = "door/letmein_wizard.html"
 	
 	def done(self, form_list, **kwargs):
-	
-	
+		"""
+		We'll get here when the form is submitted and valid.
+		1) Check member cooldown
+		2) If cooldown all good, publish the request
+		3) Return to the door status page.
+		"""
+		cleaned_data = self.get_all_cleaned_data()
+		member = self.request.unigames_member
+		cooldown_check = redis_check_cooldown(member.pk)
+		if cooldown_check:
+			publish_letmein_request(member.short_name, cleaned_data["entrance"])
+			messages.success(self.request, "Complete!")
+			return redirect("door:status")
+		else:
+			messages.error(self.request, "Failed!")
+			return redirect("door:status")
+		
